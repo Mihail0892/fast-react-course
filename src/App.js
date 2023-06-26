d5804ba4a688b378881206159c48e5e01af86564
Mihail0892 <yanamishatereshchenko@gmail.com>
2023-06-26 17:21:06 +0300

    create and remove func

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { useState } from "react";
-import './styles/App.css'
+import "./styles/App.css";
 import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 function App() {
   const [posts, setPosts] = useState([
-    {id:1, title:'Javascript', body:'Description'},
-    {id:2, title:'Javascript 2', body:'Description'},
-    {id:3, title:'Javascript 3', body:'Description'},
-  ])
-  const [posts2, setPosts2] = useState([
-    {id:1, title:'Python', body:'Description'},
-    {id:2, title:'Python 2', body:'Description'},
-    {id:3, title:'Python 3', body:'Description'},
-  ])
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 3, title: "Javascript 3", body: "Description" },
+  ]);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((item) => item.id !== post.id));
+  };
 
   return (
     <div className="App">
-      <PostList posts={posts} title="Посты про JS"/>
-      <PostList posts={posts2} title="Посты про Python"/>
-      
+      <PostForm create={createPost} />
+      {posts.length !== 0 ? (
+        <PostList remove={removePost} posts={posts} title="Посты про JS" />
+      ) : (
+        <h1 style={{textAlign:'center'}}>Посты не найдены!</h1>
+      )}
     </div>
   );
 }
